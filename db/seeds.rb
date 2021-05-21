2ca48ad413f88908a4aa421a94f2c961fc78d8c4
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-21 14:57:27 +0900

    graphql install

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  p user
+  5.times do
+    p Post.create(title: Faker::Lorem.sentence(word_count:3), content: Faker::Lorem.paragraph(sentence_count:40), user: user)
+  end
+end
