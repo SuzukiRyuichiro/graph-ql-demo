17c7eeeb60f52f84269e0c915ca97dbae71944b8
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-21 14:33:00 +0900

    posts have content

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_053118) do
+ActiveRecord::Schema.define(version: 2021_05_21_053226) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
